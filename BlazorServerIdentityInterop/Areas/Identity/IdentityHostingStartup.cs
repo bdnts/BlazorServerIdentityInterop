be791a2304a49dcf511351dfbe83375a88fa0946
Brian Douglass <corp@tps-eoltp.com>
2020-04-21 12:07:03 -0700

    --Scaffolded Identity.

---
+++ BlazorServerIdentityInterop/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BlazorServerIdentityInterop.Areas.Identity.Data;
+using BlazorServerIdentityInterop.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BlazorServerIdentityInterop.Areas.Identity.IdentityHostingStartup))]
+namespace BlazorServerIdentityInterop.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BlazorServerIdentityInteropContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("BlazorServerIdentityInteropContextConnection")));
+
+                services.AddDefaultIdentity<BlazorServerIdentityInteropUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<BlazorServerIdentityInteropContext>();
+            });
+        }
+    }
+}

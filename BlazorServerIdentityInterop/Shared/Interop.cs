9b3d91e19d380423e301872202f9621b9c695d07
Brian Douglass <corp@tps-eoltp.com>
2020-04-21 14:00:04 -0700

    --The 3 critical files for Interop usage

---
+++ BlazorServerIdentityInterop/Shared/Interop.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+//https://www.oqtane.org/Resources/Blog/PostId/527/exploring-authentication-in-blazor
+namespace BlazorServerIdentityInterop.Shared
+{
+    public class Interop
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public Interop(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public Task SetCookie(string name, string value, int days)
+        {
+            try
+            {
+                _jsRuntime.InvokeAsync<string>(
+                "interop.setCookie",
+                name, value, days);
+                return Task.CompletedTask;
+            }
+            catch
+            {
+                return Task.CompletedTask;
+            }
+        }
+
+        public ValueTask<string> GetCookie(string name)
+        {
+            try
+            {
+                return _jsRuntime.InvokeAsync<string>(
+                    "interop.getCookie",
+                    name);
+            }
+            catch
+            {
+                return new ValueTask<string>(Task.FromResult(string.Empty));
+            }
+        }
+
+        public Task UpdateTitle(string title)
+        {
+            try
+            {
+                _jsRuntime.InvokeAsync<string>(
+                    "interop.updateTitle",
+                    title);
+                return Task.CompletedTask;
+            }
+            catch
+            {
+                return Task.CompletedTask;
+            }
+        }
+
+        public Task IncludeMeta(string id, string attribute, string name, string content)
+        {
+            try
+            {
+                _jsRuntime.InvokeAsync<string>(
+                    "interop.includeMeta",
+                    id, attribute, name, content);
+                return Task.CompletedTask;
+            }
+            catch
+            {
+                return Task.CompletedTask;
+            }
+        }
+
+        public Task IncludeLink(string id, string rel, string url, string type)
+        {
+            try
+            {
+                _jsRuntime.InvokeAsync<string>(
+                    "interop.includeLink",
+                    id, rel, url, type);
+                return Task.CompletedTask;
+            }
+            catch
+            {
+                return Task.CompletedTask;
+            }
+        }
+
+        public Task IncludeScript(string id, string src, string content, string location)
+        {
+            try
+            {
+                _jsRuntime.InvokeAsync<string>(
+                    "interop.includeScript",
+                    id, src, content, location);
+                return Task.CompletedTask;
+            }
+            catch
+            {
+                return Task.CompletedTask;
+            }
+        }
+
+        public Task IncludeCSS(string id, string url)
+        {
+            try
+            {
+                _jsRuntime.InvokeAsync<string>(
+                    "interop.includeLink",
+                    id, "stylesheet", url, "text/css");
+                return Task.CompletedTask;
+            }
+            catch
+            {
+                return Task.CompletedTask;
+            }
+        }
+
+        public ValueTask<string> GetElementByName(string name)
+        {
+            try
+            {
+                return _jsRuntime.InvokeAsync<string>(
+                    "interop.getElementByName",
+                    name);
+            }
+            catch
+            {
+                return new ValueTask<string>(Task.FromResult(string.Empty));
+            }
+        }
+
+        public async Task SubmitForm(string path, object fields)
+        {
+            try
+            {
+                var response = await _jsRuntime.InvokeAsync<string>(
+                "interop.submitForm",
+                path, fields);
+                return;
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        public ValueTask<string[]> GetFiles(string id)
+        {
+            try
+            {
+                return _jsRuntime.InvokeAsync<string[]>(
+                    "interop.getFiles",
+                    id);
+            }
+            catch
+            {
+                return new ValueTask<string[]>(Task.FromResult(new string[0]));
+            }
+        }
+
+        public Task UploadFiles(string posturl, string folder, string id)
+        {
+            try
+            {
+                _jsRuntime.InvokeAsync<string>(
+                "interop.uploadFiles",
+                posturl, folder, id);
+                return Task.CompletedTask;
+            }
+            catch
+            {
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
